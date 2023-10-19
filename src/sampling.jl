3fa70ac7995e8dfac5f9b2dd5c1831e3848b045b
Mitchell Holt <mitchellholt2002@gmail.com>
2023-10-20 09:43:39 +1000

    finished customer stuff

--- src/sampling.jl
+++ src/sampling.jl
@@ -2,11 +2,11 @@
 rate_scv_gamma(rate::Float64, scv::Float64) = Gamma(1/scv, scv/rate)
 
 
-function next_arrival_duration(state::S, q::Int) where {S <: State}
+function next_arrival_duration(state::State, q::Int)
     return rand(Exponential(1/state.parameters.alpha_vector[q]))
 end
 
-function next_service_duration(state::S, q::Int) where {S <: State}
+function next_service_duration(state::State, q::Int)
     return rand(rate_scv_gamma(state.parameters.mu_vector[q], state.parameters.c_s))
 end
 
@@ -18,7 +18,7 @@ function next_on_duration(state::NetworkState)
     return rand(Exponential(1/state.parameters.gamma_2))
 end
 
-function next_location(state::NetworkState, q::Int)
+function next_location(state::State, q::Int)
     L = state.parameters.L 
     P = state.parameters.P
     weights = [P[q,:];[1 - sum(P[q,:])]]
